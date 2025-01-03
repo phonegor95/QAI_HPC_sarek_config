2ef429c08c108722b2488ea5f0e62dbb5c03af04
phonegor95 <phoenixhung2@gmail.com>
2025-01-03 15:43:19 +0800

    remove duplicated parameters in launch_nf.sh

--- launch_nf.sh
+++ launch_nf.sh
@@ -2,13 +2,6 @@
 nextflow run nf-core/sarek -profile conda \
     --input /mnt/SA127/methylation/20250102_WGS/samplesheet.csv \
     --outdir /mnt/SA127/methylation/20250102_WGS/results \
-    --aligner bwa-mem2 \
-    --trim_fastq true \
-    --joint_germline true \
-    --save_trimmed true \
-    --tools haplotypecaller \
-    --genome GATK.GRCh38 \
-    --email hongyongfeng@hkqai.hk \
     -c executor.config \
     -params-file nf-params.json \
     -bg \
