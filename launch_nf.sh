32f2d1bc7684ca2f307a0b76ac80e9ca808d68ef
phonegor95 <149480813+phonegor95@users.noreply.github.com>
2025-01-22 22:11:55 +0800

    phonegor95/sarek gatk4.6.1.0

--- launch_nf.sh
+++ launch_nf.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-nextflow run nf-core/sarek -profile conda \
+nextflow run phonegor95/sarek -profile conda \
     --input samplesheet.csv \
     --igenomes_base /mnt/8TB-HDD/hongyongfeng/igenomes_base \
     --outdir results \
     -c executor.config \
-    -params-file nf-params.json \
     -bg \
     -resume
