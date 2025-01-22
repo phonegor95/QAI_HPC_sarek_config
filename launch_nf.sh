6dcbc0d686203a429f766a48b45b7eff0ce0f3dc
phonegor95 <PHOENIXHUNG2@gmail.com>
2025-01-22 21:26:14 +0800

    mv nf-params to excutor.config

--- launch_nf.sh
+++ launch_nf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 nextflow run nf-core/sarek -profile conda \
     --input samplesheet.csv \
+    --igenomes_base /mnt/8TB-HDD/hongyongfeng/igenomes_base \
     --outdir results \
     -c executor.config \
     -params-file nf-params.json \
