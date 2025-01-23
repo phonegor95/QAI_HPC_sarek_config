6504d88ae12d63058b46751c50c2a82298f83698
hong <phoenixhung2@gmail.com>
2025-01-23 10:36:53 +0000

    use withName:'slurm' instead

--- launch_nf.sh
+++ launch_nf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nextflow run phonegor95/sarek -profile conda \
+nextflow run phonegor95/sarek -profile docker \
     -process.executor local \
     --input samplesheet.csv \
     --igenomes_base /mnt/8TB-HDD/hongyongfeng/igenomes_base \
